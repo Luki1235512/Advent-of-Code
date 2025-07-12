0203bb8c2221821dc0f1f7689345be35797ebe2a
Luki1235512 <luki1235512@gmail.com>
2025-07-12 15:12:18 +0200

    ts setup and template

---
+++ templates/tsTemplate.ts
@@ -0,0 +1,22 @@
+import * as fs from "fs";
+import * as path from "path";
+
+function readInput(): string {
+  const inputPath = path.join(__dirname, "../input/01.txt");
+  return fs.readFileSync(inputPath, "utf8").trim();
+}
+
+function part1(input: string): number {
+  return 0;
+}
+
+function part2(input: string): number {
+  return 0;
+}
+
+const input = readInput();
+  
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
+
+export { part1, part2, readInput };
