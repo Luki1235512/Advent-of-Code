a9e1b6e6d21fdc9f0925a463f9ec896e418251cf
Luki1235512 <luki1235512@gmail.com>
2025-07-12 15:50:23 +0200

    Part 1 and 2 - day 01/2015

--- 2015/TypeScript/01.ts
+++ 2015/TypeScript/01.ts
@@ -7,16 +7,30 @@ function readInput(): string {
 }
 
 function part1(input: string): number {
-  return 0;
+  let floor = 0;
+
+  for (const char of input) {
+    floor += char === "(" ? 1 : -1;
+  }
+
+  return floor;
 }
 
 function part2(input: string): number {
-  return 0;
+  let floor = 0;
+
+  for (const [index, char] of input.split("").entries()) {
+    floor += char === "(" ? 1 : -1;
+
+    if (floor === -1) {
+      return index + 1;
+    }
+  }
+
+  return floor;
 }
 
 const input = readInput();
   
 console.log("Part 1:", part1(input));
 console.log("Part 2:", part2(input));
-
-export { part1, part2, readInput };
