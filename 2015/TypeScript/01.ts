0850d8a1ede0db03e522e35c270871484f11fc18
Luki1235512 <luki1235512@gmail.com>
2025-07-12 15:58:00 +0200

    Add missing types

--- 2015/TypeScript/01.ts
+++ 2015/TypeScript/01.ts
@@ -7,7 +7,7 @@ function readInput(): string {
 }
 
 function part1(input: string): number {
-  let floor = 0;
+  let floor: number = 0;
 
   for (const char of input) {
     floor += char === "(" ? 1 : -1;
@@ -17,7 +17,7 @@ function part1(input: string): number {
 }
 
 function part2(input: string): number {
-  let floor = 0;
+  let floor: number = 0;
 
   for (const [index, char] of input.split("").entries()) {
     floor += char === "(" ? 1 : -1;
