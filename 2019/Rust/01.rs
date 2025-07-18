6d2499bcace8d4b626d918c11cd3fedb6b7841c9
Luki1235512 <luki1235512@gmail.com>
2025-07-18 18:42:12 +0200

    2019 day 1

---
+++ 2019/Rust/01.rs
@@ -0,0 +1,46 @@
+mod day_01 {
+    use std::fs;
+    use std::path::{Path, PathBuf};
+
+    fn read_input() -> String {
+        let input_path: PathBuf = Path::new(file!()).parent().unwrap().join("../input/01.txt");
+        fs::read_to_string(input_path)
+            .expect("Failed to read input file")
+            .trim()
+            .to_string()
+    }
+
+    fn part1(input: &str) -> i32 {
+        return input
+            .lines()
+            .map(|mass| {
+                let fuel = mass.parse::<i32>().unwrap();
+                (fuel / 3) - 2
+            })
+            .sum();
+    }
+
+    fn part2(input: &str) -> i32 {
+        let mut fuel = 0;
+
+        for mass in input.split("\r\n") {
+            let mut tmp_fuel = (mass.parse::<i32>().unwrap() / 3) - 2;
+
+            while tmp_fuel > 0 {
+                fuel += tmp_fuel;
+                tmp_fuel = (tmp_fuel / 3) - 2;
+            }
+        }
+
+        return fuel;
+    }
+
+    pub fn run() {
+        let input: String = read_input();
+
+        println!("Part 1: {}", part1(&input));
+        println!("Part 2: {}", part2(&input));
+    }
+}
+
+pub use day_01::run;
---
+++ 2019/TypeScript/01.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import * as path from "path";
+
+function readInput(): string {
+  const inputPath = path.join(__dirname, "../input/01.txt");
+  return fs.readFileSync(inputPath, "utf8").trim();
+}
+
+function part1(input: string): number {
+  let fuel = 0;
+
+  for (const mass of input.split("\r\n")) {
+    fuel += Math.floor((Number(mass) / 3)) - 2;
+  }
+  
+  return fuel;
+}
+
+function part2(input: string): number {
+  let fuel = 0;
+
+  for (const mass of input.split("\r\n")) {
+    let tmpFuel = Math.floor((Number(mass) / 3)) - 2;
+
+    while (tmpFuel > 0) {
+      fuel += tmpFuel;
+      tmpFuel = Math.floor((tmpFuel / 3)) - 2;
+    }
+  }
+  
+  return fuel;
+}
+
+const input = readInput();
+  
+console.log("Part 1:", part1(input));
+console.log("Part 2:", part2(input));
